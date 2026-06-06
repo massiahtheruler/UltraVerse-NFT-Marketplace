91c0c52504cd83489731e7efb8074b625f774a7a
Justin <justin.henry124@gmail.com>
2026-06-06 15:44:33 -0400

    Load hot collections from API

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getHotCollections() {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections",
+        );
+        setCollections(response.data);
+      } catch (error) {
+        console.error("Failed to load hot collections:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getHotCollections();
+  }, []);
+
+  const visibleCollections = isLoading ? new Array(6).fill(null) : collections;
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,25 +35,42 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {visibleCollections.map((collection, index) => (
+            <div
+              className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              key={collection?.id || index}
+            >
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+                  {collection && (
+                    <Link to="/item-details">
+                      <img
+                        src={collection.nftImage}
+                        className="lazy img-fluid"
+                        alt={collection.title}
+                      />
+                    </Link>
+                  )}
                 </div>
                 <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
+                  {collection && (
+                    <>
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={collection.authorImage}
+                          alt={collection.title}
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </>
+                  )}
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{collection?.title || "Loading..."}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{collection ? `ERC-${collection.code}` : "ERC"}</span>
                 </div>
               </div>
             </div>
